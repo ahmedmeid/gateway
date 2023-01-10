4dd02fd7483ee0154c4157e0abccf46eab972756
Ahmed Mohamed <ahmed.eid@gmx.com>
2023-01-10 15:03:32 +0300

    fixed kafka test container issue

--- src/test/java/com/ahmedmeid/fleet/config/TestContainersSpringContextCustomizerFactory.java
+++ src/test/java/com/ahmedmeid/fleet/config/TestContainersSpringContextCustomizerFactory.java
@@ -5,21 +5,17 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.support.DefaultSingletonBeanRegistry;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.core.annotation.AnnotatedElementUtils;
 import org.springframework.test.context.ContextConfigurationAttributes;
 import org.springframework.test.context.ContextCustomizer;
 import org.springframework.test.context.ContextCustomizerFactory;
-import org.testcontainers.containers.KafkaContainer;
 import tech.jhipster.config.JHipsterConstants;
 
 public class TestContainersSpringContextCustomizerFactory implements ContextCustomizerFactory {
 
     private Logger log = LoggerFactory.getLogger(TestContainersSpringContextCustomizerFactory.class);
 
-    private static KafkaTestContainer kafkaBean;
     private static SqlTestContainer prodTestContainer;
 
     @Override
@@ -57,24 +53,6 @@ public class TestContainersSpringContextCustomizerFactory implements ContextCust
                     testValues = testValues.and("spring.liquibase.url=" + prodTestContainer.getTestContainer().getJdbcUrl() + "");
                 }
             }
-            EmbeddedKafka kafkaAnnotation = AnnotatedElementUtils.findMergedAnnotation(testClass, EmbeddedKafka.class);
-            if (null != kafkaAnnotation) {
-                log.debug("detected the EmbeddedKafka annotation on class {}", testClass.getName());
-                log.info("Warming up the kafka broker");
-                if (null == kafkaBean) {
-                    kafkaBean = beanFactory.createBean(KafkaTestContainer.class);
-                    beanFactory.registerSingleton(KafkaTestContainer.class.getName(), kafkaBean);
-                    // ((DefaultListableBeanFactory)beanFactory).registerDisposableBean(KafkaTestContainer.class.getName(), kafkaBean);
-                }
-                testValues =
-                    testValues.and(
-                        "spring.cloud.stream.kafka.binder.brokers=" +
-                        kafkaBean.getKafkaContainer().getHost() +
-                        ':' +
-                        kafkaBean.getKafkaContainer().getMappedPort(KafkaContainer.KAFKA_PORT)
-                    );
-            }
-            testValues.applyTo(context);
         };
     }
 }
